cafbfff4c06b094f18bacd228630d17c0b9db924
Danilo Almeida <paioniu@gmail.com>
2023-04-03 14:15:07 -0300

    Inicio e-comerce php

---
+++ checkout.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Com Paypal</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <img src="assets/img/banner.jpg" alt="Banner Pet Shop" srcset="">
+    </header>
+    <main>
+        <?php
+            var_dump($_GET);
+        ?>
+    </main>
+    <footer>
+        <strong>Pet Shop Paioniu</strong><br>
+        Endereço: tal tal x <br>
+        Telefone: (XX) XXXX-XXXX
+    </footer>
+    
+    <script src="assets/js/javascript.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Com Paypal</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <img src="assets/img/banner.jpg" alt="Banner Pet Shop" srcset="">
+    </header>
+    <main>
+        <div class="product">
+            <div class="product__title">Casinha</div>
+            <div class="product__price">R$ 200,00</div>
+            <div class="product__image"><img src="assets/img/casinha.jpeg" alt="Casinha do cachorro" srcset=""></div>
+            <a href="checkout.php?name=casinha&&price=200">
+                <div class="product__button">
+                    <div class="product__button--label">Comprar</div>
+                </div>
+            </a>
+        </div>
+        
+        <div class="product">
+            <div class="product__title">Casinha</div>
+            <div class="product__price">R$ 200,00</div>
+            <div class="product__image"><img src="assets/img/casinha.jpeg" alt="Casinha do cachorro" srcset=""></div>
+            <a href="checkout.php?name=casinha&&price=200">
+                <div class="product__button">
+                    <div class="product__button--label">Comprar</div>
+                </div>
+            </a>
+        </div>
+        <div class="product">
+            <div class="product__title">Casinha</div>
+            <div class="product__price">R$ 200,00</div>
+            <div class="product__image"><img src="assets/img/casinha.jpeg" alt="Casinha do cachorro" srcset=""></div>
+            <a href="checkout.php?name=casinha&&price=200">
+                <div class="product__button">
+                    <div class="product__button--label">Comprar</div>
+                </div>
+            </a>
+        </div>
+        <div class="product">
+            <div class="product__title">Casinha</div>
+            <div class="product__price">R$ 200,00</div>
+            <div class="product__image"><img src="assets/img/casinha.jpeg" alt="Casinha do cachorro" srcset=""></div>
+            <a href="checkout.php?name=casinha&&price=200">
+                <div class="product__button">
+                    <div class="product__button--label">Comprar</div>
+                </div>
+            </a>
+        </div>
+
+    </main>
+    <footer>
+        <strong>Pet Shop Paioniu</strong><br>
+        Endereço: tal tal x <br>
+        Telefone: (XX) XXXX-XXXX
+    </footer>
+    
+    <script src="assets/js/javascript.js"></script>
+</body>
+</html>
